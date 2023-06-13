ba9e9e97fc3f3445f7511d8a84fd53d636fda823
unknown <sean.jin@teehub1.com>
2023-06-13 11:40:03 -0700

    call secret manager

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,29 @@
-import pandas as pd
+import boto3
+from botocore.exceptions import ClientError
+
 
 def lambda_handler(event, context):
-    d = {'col1' : [1,2], 'col2': [3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('Done x1')
+    secret_name = "sandbox/plaidAPI"
+    region_name = "us-west-1"
+
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        # For a list of exceptions thrown, see
+        # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+        raise e
+
+    # Decrypts secret using the associated KMS key.
+    secret = get_secret_value_response['SecretString']
+
+    # Your code goes here.
+    print(secret)
